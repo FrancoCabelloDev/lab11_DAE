b3eb406c49b8609f05199f5dfe67f3dd4fa4a663
Francys999 <francis11444@gmail.com>
2025-05-28 16:13:18 -0500

    Subida paso 16

--- src/components/modules/Hero.jsx
+++ src/components/modules/Hero.jsx
@@ -5,14 +5,18 @@ const Hero = ({ handleEvent }) => {
     <article className="hero">
       <div className="container d-flex f-direction-column a-items-center g-8">
         <h1 className="hero__title t-align-center">
-          Explore spoiler-free cinema with <span className="c-primary">AI reviews</span>
+          Explore spoiler-free cinema with{" "}
+          <span className="c-primary">AI reviews</span>
         </h1>
         <div className="d-flex f-direction-column a-items-center g-4">
           <p className="hero__paragraph t-align-center">
             Your movie app with advanced features, premieres, and interactive AR experiences.
           </p>
           <div className="flexbox flexbox--responsive g-4">
-            <a href="#now-showing" className="button button--primary interactive interactive--xl">
+            <a
+              href="#now-showing"
+              className="button button--primary interactive interactive--xl"
+            >
               🎬 Browse Movies
             </a>
             <a className="button button--outline-primary interactive interactive--xl">
